57601d826ce3e76bac6db8bf56764b9b11fb5d33
tthmaks <tthkmaks@gmail.com>
2024-02-28 16:56:21 +0200

    Add project files.

---
+++ MyModule.py
@@ -0,0 +1,25 @@
+﻿from MyModule import *
+
+while True:
+    print("Выберите действие:")
+    print("1-Регистрация\n2-Авторизация\n3-Изменение имени или пароля\n4-Восстановление пароля\n5-Завершение\n")
+    choice = input()
+    if choice == "1":
+        username = input("Введите имя пользователя: ")
+        password = input("Введите пароль: ")
+        register_user,password_user = register_user(username, password)
+    elif choice == "2":
+        username = input("Введите имя пользователя: ")
+        password = input("Введите пароль: ")
+        register_user,password_user = register_user(username, password)
+    elif choice =="3":
+        username = input("Введите текущее имя пользователя: ")
+        new_username = input("Введите новое имя пользователя: ")
+        new_password = input("Введите новый пароль: ")
+        register_user,password_user = register_user(username, new_username, new_password)
+    elif choice == "4":
+        username = input("Введите имя пользователя для восстановления пароля: ")
+        register_user,password_user = reset_password(username)
+    elif choice =="5":
+        print("Завершение.")
+        break
---
+++ Registreerimine_ja_autoriseerimine.py
@@ -0,0 +1,58 @@
+﻿register_user = []
+password_user = []
+
+
+def register_user(username, password)->any:
+    """
+    Регистрация нового пользователя.
+
+    Parameters:
+    username: Имя пользователя.
+    password: Пароль пользователя.
+    """
+    register_user.append({'username': username, 'password': password})
+    print(f"Пользователь {username} успешно зарегистрирован.")
+def vhod_user(username, password)->bool:
+    """
+    Авторизация пользователя.
+
+    Parameters:
+    username: Имя пользователя.
+    password: Пароль пользователя.
+
+    Returns:
+    bool: Возвращает True, иначе False.
+    """
+    for user in register_user:
+        if user['username'] == username and user['password'] == password:
+            print(f"Пользователь {username} успешно авторизован.")
+            return True
+    print("Ошибка авторизации. Проверьте имя пользователя и пароль.")
+    return False
+def change_user(username, new_username, new_password)->any:
+    """
+    Изменение имени пользователя или пароля.
+
+    Parameters:
+    username: Имя текущего пользователя.
+    new_username: Новое имя пользователя.
+    new_password: Новый пароль пользователя.
+    """
+    for i in register_user:
+        if i['username'] == username:
+            i['username'] = new_username
+            i['password'] = new_password
+            print(f"успешно изменены.")
+            return
+    print(f"Пользователь {username} не найден.")
+def reset_password(username)->any:
+    """Восстановление пароля пользователя.
+    Parameters:
+    username: Имя пользователя, пароль которого нужно сбросить.
+    """
+    for i in register_user:
+        if i['username'] == username:
+            new_password = reset_password()
+            print(f"Пароль {username} сброшен.")
+            return new_password
+   
